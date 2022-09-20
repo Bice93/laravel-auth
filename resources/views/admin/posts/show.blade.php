0560b02b212b2946dc8f34bf9410259f8275a02b
Bice93 <bianca.cerulo@hotmail.it>
2022-09-20 12:31:39 +0200

    Edit of the card in show.blade

--- resources/views/admin/posts/show.blade.php
+++ resources/views/admin/posts/show.blade.php
@@ -4,11 +4,21 @@
     <div class="container">
         <div class="row">
             <div class="col-6 offset-md-3">
-                <div class="card">
+                <div class="card p-3">
                     <img src="{{ $post->post_image }}" class="card-img-top" alt="{{ $post->title }}'s image">
                     <div class="card-body">
                         <h4>{{ $post->title }}</h4>
                         <p class="card-text">{{ $post->post_content }}</p>
+                    </div>
+                    <div class="col-6 offset-md-3 text-center">
+                        <a href="{{ route('admin.posts.edit', $post->id) }}"
+                            class="btn btn-sm btn-success">Edit</a>
+                        <form class="d-inline" action="{{ route('admin.posts.destroy', $post->id) }}"
+                            method="POST">
+                            @csrf
+                            @method('DELETE')
+                            <button type="submit" class="btn btn-sm btn-danger">DELETE</button>
+                        </form>
                     </div>
                 </div>
             </div>
